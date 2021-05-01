00bcb0b676ece4e72ddc6a0b59443771224e9ae8
harshitagupta1512 <83108764+harshitagupta1512@users.noreply.github.com>
2021-05-02 01:15:06 +0530

    Update projectMain.c

--- projectMain.c
+++ projectMain.c
@@ -1,1 +1,25 @@
+#include "project.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
+int main(void)
+{
+    int numPlaces,numStreets;
+    scanf("%d %d",&numPlaces,&numStreets);
+    Graph* City=CreateGraph(numPlaces);
+
+    for(int i=0;i<numStreets;i++)
+    {
+        int src,dest;
+        struct StreetData SD;
+        scanf("%d %d %d %d %d %d %d",src,dest,SD.length,SD.numLanes,SD.num_cars,SD.num_accidents,SD.speed_limit);
+        AddStreet(src,dest,SD);
+    }
+
+    //-------------City graph is made---------------------//
+
+    int finalSource, finalDestination;
+    scanf("%d %d",&finalSource,&finalDestination);
+    getFastestPath(City,finalSource,finalDestination);
+}
