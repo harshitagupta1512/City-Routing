09d42540cac8a5c971c557659df019e27130bd1d
harshitagupta1512 <83108764+harshitagupta1512@users.noreply.github.com>
2021-05-09 12:40:30 +0530

    Add files via upload

---
+++ testCaseGenerator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+    int a, b, c, d, e;
+    int x;
+    int n;
+    srand(time(0));
+    printf("enter number of edges:");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        a = rand() % 100 + 1;
+        b = rand() % 4 + 1;
+        c = rand() % 291 + 10;
+        d = rand() % 10 + 1;
+        e = rand() % 91 + 10;
+        x = 0.4 * a + 0.6 * (0.4 * c + 1 / (b * 6.00) + 1 / (e * 0.4));
+        printf("%3d %3d %3d %3d %3d ---> weight=%d\n", a, b, c, d, e, x);
+    }
+}
